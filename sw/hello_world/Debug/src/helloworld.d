293066829b042cff22c634b975cf19936578dd2f
Jeramie Lee Vens <vens@linux-4.ece.iastate.edu>
2014-09-03 18:04:07 -0500

    Pain and suffering

---
+++ sw/hello_world/Debug/src/helloworld.d
@@ -0,0 +1,12 @@
+src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../hello_world_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../hello_world_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../hello_world_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
